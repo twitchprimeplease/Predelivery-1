1353f1e8fede518dc0b88fc116a3a4b7ca5b7ba4
Melanie Santos ospitia <melaniesantosospitia@MacBook-Pro-de-Melanie.local>
2022-08-27 10:25:57 -0500

    aH

---
+++ public-app/sketch.js
@@ -0,0 +1,85 @@
+const NGROK = `https://${window.location.hostname}`;
+console.log('Server IP: ', NGROK);
+let socket = io(NGROK, { path: '/real-time' });
+
+let controllerX, controllerY = 0;
+let interactions = 2;
+let isTouched = false;
+
+function setup() {
+    frameRate(60);
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.style('z-index', '-1');
+    canvas.style('position', 'fixed');
+    canvas.style('top', '0');
+    canvas.style('right', '0');
+    controllerX = windowWidth / 2;
+    controllerY = windowHeight / 2;
+    background(0);
+    angleMode(DEGREES);
+
+    socket.emit('device-size', {windowWidth, windowHeight});
+
+    let btn = createButton("Permitir movimiento");
+	btn.mousePressed(function(){
+		DeviceOrientationEvent.requestPermission();
+	});
+
+}
+
+function draw() {
+    background(0, 5);
+    newCursor(pmouseX, pmouseY);
+    fill(255);
+    ellipse(controllerX, controllerY, 50, 50);
+}
+
+/*function mouseDragged() {
+    socket.emit('positions', { controlX: pmouseX, controlY: pmouseY });
+}*/
+
+function touchMoved() {
+    switch (interactions) {
+        case 0:
+            socket.emit('mobile-instructions', { interactions, pmouseX, pmouseY });
+            background(255, 0, 0);
+            break;
+    }
+}
+
+function touchStarted(){
+    isTouched = true;
+}
+
+function touchEnded(){
+    isTouched = false;
+}
+
+function deviceMoved() {
+    switch (interactions) {
+        case 1:
+            socket.emit('mobile-instructions', { interactions, pAccelerationX, pAccelerationY, pAccelerationZ });
+            background(0, 255, 255);
+            break;
+        case 2:
+            socket.emit('mobile-instructions', { interactions, rotationX, rotationY, rotationZ });
+            background(0, 255, 0);
+            break;
+    }
+    
+}
+
+function deviceShaken() {
+    //socket.emit('mobile-instructions', 'Moved!');
+    //background(0, 255, 255);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function newCursor(x, y) {
+    noStroke();
+    fill(255);
+    ellipse(x, y, 10, 10);
+}
---
+++ public-mupi/sketch.js
@@ -0,0 +1,79 @@
+const NGROK = `https://${window.location.hostname}`;
+let socket = io(NGROK, { path: '/real-time' });
+console.log('Server IP: ', NGROK);
+
+let controllerX, controllerY = 0;
+let deviceWidth, deviceHeight = 0;
+let mupiWidth, mupiHeight = 0;
+let ballSize = 20;
+
+function setup() {
+    frameRate(60);
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.style('z-index', '-1');
+    canvas.style('position', 'fixed');
+    canvas.style('top', '0');
+    canvas.style('right', '0');
+    controllerX = windowWidth / 2;
+    controllerY = windowHeight / 2;
+    mupiWidth = windowWidth;
+    mupiHeight = windowHeight;
+    background(0);
+}
+
+function draw() {
+    background(0, 5);
+    newCursor(pmouseX, pmouseY);
+    fill(255);
+    ellipse(controllerX, controllerY, ballSize, ballSize);
+
+}
+
+function mouseDragged() {
+    socket.emit('positions', { controlX: pmouseX, controlY: pmouseY });
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function newCursor(x, y) {
+    noStroke();
+    fill(255);
+    ellipse(x, y, 10, 10);
+}
+
+socket.on('mupi-instructions', instructions => {
+    console.log('ID: ' + socket.id);
+
+    let { interactions } = instructions;
+    switch (interactions) {
+        case 0:
+            let { pmouseX, pmouseY } = instructions;
+            controllerX = (pmouseX * mupiWidth) / deviceWidth;
+            controllerY = (pmouseY * mupiHeight) / deviceHeight;
+            console.log({ controllerX, controllerY });
+            break;
+        case 1:
+            let { pAccelerationX, pAccelerationY, pAccelerationZ } = instructions;
+            ballSize = pAccelerationY < 0 ? pAccelerationY * -2 : pAccelerationY * 2;
+            break;
+        case 2:
+            let { rotationX, rotationY, rotationZ } = instructions;
+            controllerY = (rotationX * mupiHeight) / 90;
+            controllerX = (rotationY * mupiWidth) / 90;
+            break;
+        case 3:
+            
+            break;
+    }
+
+
+});
+
+socket.on('mupi-size', deviceSize => {
+    let { windowWidth, windowHeight } = deviceSize;
+    deviceWidth = windowWidth;
+    deviceHeight = windowHeight;
+    console.log(`User is using a smartphone size of ${deviceWidth} and ${deviceHeight}`);
+});
